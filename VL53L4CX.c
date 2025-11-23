4466cad7f7feb8aad13eb5b485653126efc653b4
Nate <nsmilelr@andrew.cmu.edu>
2025-11-23 01:26:32 -0500

    Successfully reads sensor module ID

--- VL53L4CX.c
+++ VL53L4CX.c
@@ -1,7 +1,7 @@
 #include <msp430.h>
 
 
-#define VL53L4CXADDR 0x52         // address of sensor is 52hex
+#define VL53L4CXADDR 0x29         // address of sensor is 52hex
 
 
 
@@ -10,7 +10,7 @@ int VL53L4CX_setup() {
     UCB0CTLW0 = UCSWRST;                // put eUSCI_B in reset state
 
     UCB0CTLW0 |= UCMODE_3 | UCMST | UCSSEL__SMCLK;      // I2C mode | master mode | SMCLK
-    UCB0BRW = 0x02;                       // baud rate = SMCLK / 1
+    UCB0BRW = 0x0A;                       // 0x02 -> 200kHz, 0x0A -> 100kHz
     UCB0CTLW1 |= UCASTP_2;              // automatic STOP assertion
     UCB0TBCNT = 0x07;                   // TX 2 bytes of data
 
@@ -19,6 +19,7 @@ int VL53L4CX_setup() {
 
 
     UCB0CTLW0 &= ~UCSWRST;               // eUSCI_B in operational state
+    UCB0IFG = 0;
     //UCB0IE |= UCTXIE;                   // enable TX-interrupt
     return 0;
 }
@@ -38,27 +39,23 @@ int verify_VL53L4CX() {
 
 
     UCB0CTLW0 |= UCTR | UCTXSTT;    // Set transmit and send start
-    //UCB0CTLW0 |= UCTXSTT;
 
     while (!(UCB0IFG & UCTXIFG));   // Wait for tx buf ready
     UCB0TXBUF = reg >> 8;           // Put data in tx buf
     while (!(UCB0IFG & UCTXIFG));   // Wait for tx buf ready
     UCB0TXBUF = reg;                // Put data in tx buf
-    
-
     while (!(UCTXIFG0 & UCB0IFG));  // Wait for transmission
 
 
     UCB0CTLW0 &= ~UCTR;             // Set to recieve mode
     UCB0CTLW0 |= UCTXSTT;           // Send start
-
-    while (!(UCTXIFG0 & UCB0IFG));  // Wait start to transmit
-
-
     while(!(UCRXIFG0 & UCB0IFG));
     read_value = UCB0RXBUF;
-
+    while(!(UCRXIFG0 & UCB0IFG));
+    UCB0CTLW0 |= UCTXNACK;          // Send NACK
     UCB0CTLW0 |= UCTXSTP;           // Send stop
+    read_value = UCB0RXBUF;
+
 
 
     // Reset I2C interupts
